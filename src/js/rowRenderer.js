matthewvasallo/angular-grid

commit 1903d26fbc68417490f881772270f046bc909a37

1) when we start editing a cell, make sure that it is visible, scrolling if necessary. 2) allow the column definition to specify a wider width for the edit template.

src/js/rowRenderer.js
@@ -1390,6 +1390,12 @@ RowRenderer.prototype.startEditing = function(eGridCell, column, node, $childSco
             }
         }
     });
+
+    var visibleColumns = this.columnModel.getDisplayedColumns();
+    var index = visibleColumns.indexOf(column);
+    if (index) {
+        this.gridPanel.ensureColIndexVisible(index, column.colDef.editWidth);
+    }
 };
 
 RowRenderer.prototype.findNextByParameters = function(rowIndex, column, params) {
@@ -1444,16 +1450,17 @@ RowRenderer.prototype.findNextByParameters = function(rowIndex, column, params) 
 
         var coordinate = position[which];
         var other = which === "x" ? "y" : "x";
+        var advanceAtEnd = params.advanceAtEnd && other.max > other.min;
 
         coordinate.current += delta;
         if (coordinate.current < coordinate.min) {
             coordinate.current = coordinate.max;
-            if (params.advanceAtEnd) {
+            if (advanceAtEnd) {
                 adjustPosition(other, -1);
             }
         } else if (coordinate.current > coordinate.max) {
             coordinate.current = coordinate.min;
-            if (params.advanceAtEnd) {
+            if (advanceAtEnd) {
                 adjustPosition(other, 1);
             }
         }

src/js/gridPanel/gridPanel.js
@@ -67,7 +67,7 @@ GridPanel.prototype.ensureIndexVisible = function(index) {
     // otherwise, row is already in view, so do nothing
 };
 
-GridPanel.prototype.ensureColIndexVisible = function(index) {
+GridPanel.prototype.ensureColIndexVisible = function(index, widthToUse) {
     if (typeof index !== 'number') {
         console.warn('col index must be a number: ' + index);
         return;
@@ -94,7 +94,7 @@ GridPanel.prototype.ensureColIndexVisible = function(index) {
         colLeftPixel += columns[i].actualWidth;
     }
 
-    var colRightPixel = colLeftPixel + column.actualWidth;
+    var colRightPixel = colLeftPixel + (widthToUse || column.actualWidth);
 
     var viewportLeftPixel = this.eBodyViewport.scrollLeft;
     var viewportWidth = this.eBodyViewport.offsetWidth;

commit 0dad50fcd7f84f260ab8a735ce503d2d21515436

fix bug in advanceAtEnd introduced by 1903d26f.

src/js/rowRenderer.js
@@ -1452,7 +1452,7 @@ RowRenderer.prototype.findNextByParameters = function(rowIndex, column, params) 
 
         var coordinate = position[which];
         var other = which === "x" ? "y" : "x";
-        var advanceAtEnd = params.advanceAtEnd && other.max > other.min;
+        var advanceAtEnd = params.advanceAtEnd && position[other].max > position[other].min;
 
         coordinate.current += delta;
         if (coordinate.current < coordinate.min) {